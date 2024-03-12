19df15d5540d3c884035ce4012fb75570cc64bb8
Eunsong Koh <eka74@sfu.ca>
2024-03-12 13:30:41 -0700

    Slight updates

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -15,16 +15,15 @@ function About() {
         <h3>Hi, My name is Song!</h3>
         <br style={{ lineHeight: "1px" }} />
         <h4>
-         I'm a 2nd
-          Year Computing Science student at Simon Fraser University. I'm
-          passionate about all things technology! I think it's awesome how we
-          can bring creative ideas and solutions to real-world problems with
+          I'm a 2nd Year Computing Science student at Simon Fraser University.
+          I'm passionate about all things technology! I think it's awesome how
+          we can bring creative ideas and solutions to real-world problems with
           something as technical as a computer.
         </h4>
         <br></br>
         <h4>
-          Outside of programming, I love <b>digital design</b>. Currently, I'm:{" "}
-          <br></br>
+          Outside of programming, I love <b>digital design</b>. Currently, I'm
+          the: <br></br>
           <b>- Director of Communication and Multimedia</b> @ SFU Developers and
           Systems Club <br></br>
           <b>- Video Editor</b> @ SFU Computing Science Student Society (CSSS)
